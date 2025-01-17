9adfc4c21f8b9b7b5e155e0c756f84c4a93ee337
sorayaskavinski <soraya1br@gmail.com>
2025-01-17 18:50:40 -0300

    ..

--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -37,17 +37,23 @@ public class TakingTurnsQueue
         {
             throw new InvalidOperationException("No one in the queue.");
         }
-        else
+
+        Person person = _people.Dequeue();
+
+        if (person.Turns == 0 || person.Turns < 0)
         {
-            Person person = _people.Dequeue();
+            _people.Enqueue (person);
+        }
+
+        else 
+        {
             if (person.Turns > 1)
             {
                 person.Turns -= 1;
-            _people.Enqueue(person);
-        }     
-
-            return person;
-        }
+                _people.Enqueue(person);
+            }           
+        }  
+        return person;        
     }        
 
     public override string ToString()
