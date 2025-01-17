6a4a4c5a6baeda1f2131d14fdfa70bc49c6078ca
sorayaskavinski <soraya1br@gmail.com>
2025-01-17 19:37:47 -0300

    final

--- week02/code/TakingTurnsQueue_Tests.cs
+++ week02/code/TakingTurnsQueue_Tests.cs
@@ -2,7 +2,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 // I'm not really sure where should I write what I did, but I wrote on top of each part of the TakingTurnsQueue 
 //Before handling with the errors <Sue> would appear where <Bob> should be so the program wasn´t Dequeue properly
 //TakingTurnsQueue's length was counting the length of the name, so I changed to .Count to count the turns of the name
-//This file didn't have the infinite loop as well.
+//This file didn't have the infinite loop as well, so it wasn't handling when turns were negative
+//after correcting those things and properly naming _people - Queue<People> _people= new() - the tests passed debugging
 
 
 
